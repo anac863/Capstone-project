e9557fff8ecc814c97f31e026752b4998780e042
anac863 <anac863@yahoo.com>
2024-11-24 23:16:26 -0500

    Update capscript.js

--- capscript.js
+++ capscript.js
@@ -9,10 +9,11 @@ form.addEventListener("submit", async (event) => {
     const phone = document.getElementById("phone").value;
 
     try {
-        const res = await fetch("<YOUR_DATABASE_ENDPOINT>", {
+        const res = await fetch("https://drhjvpjofusizdimkkbq.supabase.co", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyaGp2cGpvZnVzaXpkaW1ra2JxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1MDc0NzYsImV4cCI6MjA0ODA4MzQ3Nn0.7mcKNAnTeBzXnduJstKdmvi2rxD-KQYOW50ZorGkQQU",
             },
             body: JSON.stringify({ name, email, phone }),
         });
